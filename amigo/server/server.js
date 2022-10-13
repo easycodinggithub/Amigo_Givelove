87bf82dc857e5391aaba0418cb8d1a69373c3eb2
bomin-1206 <gbm4655@naver.com>
2022-10-13 19:23:16 +0900

    givelove

---
+++ amigo/server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const test = require("./routes/test");
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use("/api", test);
+
+const port = 5000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
